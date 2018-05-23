c2f792ad31d217cb2905856b62c934b2f3d3c102
Archit Kapoor <archit.imsec10@gmail.com>
2018-05-23 17:13:40 +0530

    Utils package - which intends to contains all the common utility functions across this aplication

---
+++ src/utils/os_utils.py
@@ -0,0 +1,17 @@
+import os
+
+
+# Check if a given file exists or not
+def file_exists(file_path):
+    file_path = str(file_path).strip()
+    return os.path.isfile(file_path) or \
+           Path().is_file(file_path)
+
+
+# Check if a given directory/folder exists or not
+def directory_exists(dir_path):
+    return os.path.exists(dir_path)
+
+
+
+
