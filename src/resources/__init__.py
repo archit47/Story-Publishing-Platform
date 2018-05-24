5f8cacbf2cec172fe5789b4e854e7da87d8a8f1e
Archit Kapoor <archit.imsec10@gmail.com>
2018-05-24 12:33:53 +0530

    Exporting the various classes defined in the resources package

---
+++ src/resources/__init__.py
@@ -0,0 +1,5 @@
+from .author_resource import AuthorResource, NewAuthorResource
+from .draft_resource import DraftResource, NewDraftResource, DraftsListResource
+from .story_resource import StoryResource, NewStoryResource
+from .feed_resource import UserFeedResource
+from .base_resource import BaseResource
